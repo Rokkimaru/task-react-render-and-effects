55be847eeb5979e02c75d7126a05b434826909da
alexandr <frexeman2001@yandex.ru>
2022-05-29 13:47:20 +0300

    task-react-render-and-effects

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,24 @@
 import { subscribe, unsubscribe } from './resources/API';
+import { useEffect, useState } from 'react';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [message, setMessage] = useState(-1);
+
+    function callback(message: number) {
+        setMessage(message);
+    }
+
+    useEffect(() => {
+        subscribe(props.sourceId, callback);
+        return () => {
+            unsubscribe(props.sourceId, callback);
+            setMessage(-1);
+        };
+    }, [props.sourceId]);
+
+    return (
+        <div>
+            {props.sourceId}:{message}
+        </div>
+    );
 }
